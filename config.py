f7978af93480e9f8efa6078465697753d58aa0a9
Rakesh163-213 <naampata35217793@gmail.com>
2025-05-31 00:22:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = 1234567         # Replace with your API ID
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID =        # Replace with your API ID
+API_HASH = ""
+BOT_TOKEN = ""
