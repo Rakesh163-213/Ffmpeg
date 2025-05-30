5f16540aab74a338f6b1ceffef4195f05ac3234b
Rakesh163-213 <naampata35217793@gmail.com>
2025-05-31 00:28:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-API_ID =        # Replace with your API ID
-API_HASH = ""
-BOT_TOKEN = ""
+import os
+
+# Pull from environment variables (Koyeb or local)
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
