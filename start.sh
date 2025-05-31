6d5fdc054d12a41d0a4c370ae312d37bc1b73e04
Rakesh163-213 <naampata35217793@gmail.com>
2025-05-31 11:57:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,5 @@
 ##!/bin/bash
 
 echo "🔥 Starting the bot..."
-
-# Any setup you want here
-# e.g., export ENV vars if not using .env
-
-# Finally, run the bot
-python main.py
-echo "Starting the bot"
 python main.py &
 python app.py
